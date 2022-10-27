dc8143ba969d09834da7273a8b8354599f000ee4
MysticalDevil <jinpeng7981@outlook.com>
2022-10-27 21:00:47 +0800

    Integrated Gin

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	r := gin.New()
+
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World",
+		})
+	})
+
+	// Handling 404 requests
+	r.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "Page return 404")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "This route is not defined, please confirm whether the url and request method ar correct",
+			})
+		}
+	})
+
+	r.Run(":8080")
 }
