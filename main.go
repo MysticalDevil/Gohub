4115f97511078ddd2fdce79accba34a787dbc876
MysticalDevil <jinepng7981@outlook.com>
2022-10-27 21:34:53 +0800

    Initialize route

--- main.go
+++ main.go
@@ -1,35 +1,19 @@
 package main
 
 import (
-	"net/http"
-	"strings"
-
 	"github.com/gin-gonic/gin"
+	"gohub/bootstrap"
+	"log"
 )
 
 func main() {
-	r := gin.New()
-
-	r.Use(gin.Logger(), gin.Recovery())
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World",
-		})
-	})
-
-	// Handling 404 requests
-	r.NoRoute(func(c *gin.Context) {
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			c.String(http.StatusNotFound, "Page return 404")
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "This route is not defined, please confirm whether the url and request method ar correct",
-			})
-		}
-	})
-
-	r.Run(":8080")
+	router := gin.New()
+	// Initialize route binding
+	bootstrap.SetupRoute(router)
+	// Run serve
+	err := router.Run(":3000")
+	if err != nil {
+		// Error handling, port occupied or other errors
+		log.Println(err.Error())
+	}
 }
