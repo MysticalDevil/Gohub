b35b77396139e7f1e98303d188c5e71b45a10d97
MysticalDevil <jinepng7981@outlook.com>
2022-10-31 22:31:04 +0800

    API: Image verification code

---
+++ app/http/controllers/api/v1/auth/verify_code_controller.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/pkg/captcha"
+	"gohub/pkg/logger"
+	"net/http"
+)
+
+// VerifyController  User controller
+type VerifyController struct {
+	v1.BaseAPIController
+}
+
+// ShowCaptcha Show image verification code
+func (vc *VerifyController) ShowCaptcha(c *gin.Context) {
+	// Generate verification code
+	id, b64s, err := captcha.NewCaptcha().GenerateCaptcha()
+	logger.LogIf(err)
+	c.JSON(http.StatusOK, gin.H{
+		"captcha_id":    id,
+		"captcha_image": b64s,
+	})
+}
