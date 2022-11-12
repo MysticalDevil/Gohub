e5615144ee9e634f7c2d89009d59aea3677bf883
MysticalDevil <jinepng7981@outlook.com>
2022-11-12 13:05:58 +0800

    Email + Email verification code registration

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -64,7 +64,7 @@ type SignupUsingPhoneRequest struct {
 
 func SignupUsingPhone(data any, c *gin.Context) map[string][]string {
 	rules := govalidator.MapData{
-		"phone":            []string{"required", "digits:11"},
+		"phone":            []string{"required", "digits:11", "not_exists:users,phone"},
 		"name":             []string{"required", "alpha_num", "between:3,20", "not_exists:users,name"},
 		"password":         []string{"required", "min:6"},
 		"password_confirm": []string{"required"},
@@ -99,6 +99,59 @@ func SignupUsingPhone(data any, c *gin.Context) map[string][]string {
 	_data := data.(*SignupUsingPhoneRequest)
 	errs = validators.ValidatePasswordConfirm(_data.Password, _data.PasswordConfirm, errs)
 	errs = validators.ValidateVerifyCode(_data.Phone, _data.VerifyCode, errs)
+
+	return errs
+}
+
+// SignupUsingEmailRequest Request information via email registration
+type SignupUsingEmailRequest struct {
+	Email           string `json:"email,omitempty" valid:"email"`
+	VerifyCode      string `json:"verify_code,omitempty" valid:"verify_code"`
+	Name            string `json:"name" valid:"name"`
+	Password        string `json:"password,omitempty" valid:"password"`
+	PasswordConfirm string `json:"password_confirm,omitempty" valid:"password_confirm"`
+}
+
+func SignupUsingEmail(data any, c *gin.Context) map[string][]string {
+	rules := govalidator.MapData{
+		"email":            []string{"required", "min:4", "max:30", "email", "not_exists:users,email"},
+		"name":             []string{"required", "alpha_num", "between:3,20", "not_exists:users,name"},
+		"password":         []string{"required", "min:6"},
+		"password_confirm": []string{"required"},
+		"verify_code":      []string{"required", "digits:6"},
+	}
+
+	messages := govalidator.MapData{
+		"email": []string{
+			"required:Email is required",
+			"min:Email length must be greater than 4",
+			"max:Email length must be less than 30",
+			"email:The email format is incorrect, please provide a valid email address",
+			"not_exists:Email is occupied",
+		},
+		"name": []string{
+			"required:Username is required",
+			"alpha_num:Username is malformed, only numbers and English are allowed",
+			"between:Username length must be between 3 and 20",
+		},
+		"password": []string{
+			"required:Password is required",
+			"min:Password length must be greater than 6",
+		},
+		"password_confirm": []string{
+			"required:Password confirm is required",
+		},
+		"verify_code": []string{
+			"required:Verification code answer is required",
+			"digits:The verification code must be a 6-digit number",
+		},
+	}
+
+	errs := validate(data, rules, messages)
+
+	_data := data.(*SignupUsingEmailRequest)
+	errs = validators.ValidatePasswordConfirm(_data.Password, _data.PasswordConfirm, errs)
+	errs = validators.ValidateVerifyCode(_data.Email, _data.VerifyCode, errs)
 
 	return errs
 }
