d3b1352e63d4c2c15e35126dead3059af5e777c9
MysticalDevil <jinpeng7981@outlook.com>
2022-11-29 16:31:14 +0800

    Command 'make model'

---
+++ app/cmd/make/make_model.go
@@ -0,0 +1,32 @@
+package make
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"gohub/pkg/console"
+	"os"
+)
+
+var CmdMakeModel = &cobra.Command{
+	Use:   "model",
+	Short: "Create model file, example: make model user",
+	Run:   runMakeModel,
+	Args:  cobra.ExactArgs(1),
+}
+
+func runMakeModel(cmd *cobra.Command, args []string) {
+	// format the model name and return a Model object
+	model := makeModelFromString(args[0])
+
+	// make sure the models directory exists, e.g. `app/models/user`
+	dir := fmt.Sprintf("app/models/%s/", model.PackageName)
+
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		console.Exit(err.Error())
+	}
+
+	createFileFromStub(dir+model.PackageName+"_model.go", "model/model", model)
+	createFileFromStub(dir+model.PackageName+"_util.go", "model/model_util", model)
+	createFileFromStub(dir+model.PackageName+"_hooks.go", "model/model_hooks", model)
+}
