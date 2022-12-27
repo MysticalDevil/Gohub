05417914fe5104ad6948efb2177ab6cd958d5759
MysticalDevil <jinpeng7981@outlook.com>
2022-12-27 21:06:58 +0800

    Validating paging requests

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 	"gohub/app/models/user"
+	"gohub/app/requests"
 	"gohub/pkg/auth"
 	"gohub/pkg/response"
 )
@@ -19,6 +20,11 @@ func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 
 // Index All user
 func (ctrl *UsersController) Index(c *gin.Context) {
+	request := requests.PaginationRequest{}
+	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+		return
+	}
+
 	data, pager := user.Paginate(c, 10)
 	response.JSON(c, gin.H{
 		"data":  data,
---
+++ app/requests/pagination_request.go
@@ -0,0 +1,34 @@
+package requests
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+)
+
+type PaginationRequest struct {
+	Sort    string `valid:"sort" form:"sort"`
+	Order   string `valid:"order" form:"order"`
+	PerPage string `valid:"per_page" from:"per_page"`
+}
+
+func Pagination(data any, c *gin.Context) map[string][]string {
+	rules := govalidator.MapData{
+		"sort":     []string{"in:id,created_at,updated_at"},
+		"order":    []string{"in:asc,desc"},
+		"per_page": []string{"numeric_between:2,100"},
+	}
+
+	messages := govalidator.MapData{
+		"sort": []string{
+			"in:Sort fields only support id, created_at, updated_at",
+		},
+		"order": []string{
+			"in:Sort fields only support asc (positive order), desc (reverse order)",
+		},
+		"per_page": []string{
+			"numeric_between:The number of entries per page has a value between 2 and 100",
+		},
+	}
+
+	return validate(data, rules, messages)
+}
