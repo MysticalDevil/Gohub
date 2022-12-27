6810c353051cdea5016685fea6d586c70ac629c3
MysticalDevil <jinpeng7981@outlook.com>
2022-12-27 21:24:35 +0800

    Category Model

---
+++ app/models/category/category_util.go
@@ -0,0 +1,40 @@
+package category
+
+import (
+	"github.com/gin-gonic/gin"
+	"gohub/pkg/app"
+	"gohub/pkg/database"
+	"gohub/pkg/paginator"
+)
+
+func Get(idStr string) (category Category) {
+	database.DB.Where("id", idStr).First(&category)
+	return
+}
+
+func GetBy(field, value string) (category Category) {
+	database.DB.Where("? = ?", field, value).First(&category)
+	return
+}
+
+func All() (categories []Category) {
+    database.DB.Find(&categories)
+    return
+}
+
+func IsExist(field, value string) bool {
+    var count int64
+    database.DB.Model(Category{}).Where("? = ?", field, value).Count(&count)
+    return count > 0
+}
+
+func Paginate(c *gin.Context, perPage int) (categories []Category, paging paginator.Paging) {
+    paging = paginator.Paginate(
+        c,
+        database.DB.Model(Category{}),
+        &categories,
+        app.V1URL(database.TableName(&Category{})),
+        perPage,
+    )
+    return
+}
---
+++ database/migrations/2022_12_27_211654_add_categories_table.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"database/sql"
+	"gohub/app/models"
+	"gohub/pkg/migrate"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+	type Category struct {
+		models.BaseModel
+
+		Name        string `gorm:"type:varchar(255);not null;index"`
+		Description string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Category{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Category{})
+	}
+
+	migrate.Add("2022_12_27_211654_add_categories_table", up, down)
+}
