759dda3481a11b0c553eaab5956819df75ea076e
MysticalDevil <jinpeng7981@outlook.com>
2023-01-16 16:00:41 +0800

    Command 'cache forget'

--- app/cmd/cache.go
+++ app/cmd/cache.go
@@ -1,27 +1,46 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
-	"gohub/pkg/cache"
-	"gohub/pkg/console"
+    "fmt"
+    "github.com/spf13/cobra"
+    "gohub/pkg/cache"
+    "gohub/pkg/console"
 )
 
 var Cache = &cobra.Command{
-	Use:   "cache",
-	Short: "Cache management",
+    Use:   "cache",
+    Short: "Cache management",
 }
 
 var CacheClear = &cobra.Command{
-	Use:   "clear",
-	Short: "Clear cache",
-	Run:   runCacheClear,
+    Use:   "clear",
+    Short: "Clear cache",
+    Run:   runCacheClear,
 }
 
+var CacheForget = &cobra.Command{
+    Use:   "forget",
+    Short: "Delete redis key, example: cache forget cache-key",
+    Run:   runCacheForget,
+}
+
+// Options for the forget Command
+var cacheKey string
+
 func init() {
-	Cache.AddCommand(CacheClear)
+    Cache.AddCommand(CacheClear, CacheForget)
+
+    // Set options for the cache forget command
+    CacheForget.Flags().StringVarP(&cacheKey, "key", "k", "", "KEY of the cache")
+    _ = CacheForget.MarkFlagRequired("key")
 }
 
 func runCacheClear(cmd *cobra.Command, args []string) {
-	cache.Flush()
-	console.Success("Cache cleared.")
+    cache.Flush()
+    console.Success("Cache cleared.")
+}
+
+func runCacheForget(cmd *cobra.Command, args []string) {
+    cache.Forget(cacheKey)
+    console.Success(fmt.Sprintf("Cache key [%s] deleted.", cacheKey))
 }
