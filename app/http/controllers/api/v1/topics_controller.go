92e81ae0b517e4029a1de7015a2cc2e9c1fdce31
MysticalDevil <jinpeng7981@outlook.com>
2023-01-16 14:25:42 +0800

    Index topic

--- app/http/controllers/api/v1/topics_controller.go
+++ app/http/controllers/api/v1/topics_controller.go
@@ -83,3 +83,16 @@ func (ctrl *TopicsController) Delete(c *gin.Context) {
 
 	response.Abort500(c, "Failed to delete, please try later~")
 }
+
+func (ctrl *TopicsController) Index(c *gin.Context) {
+	request := requests.PaginationRequest{}
+	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+		return
+	}
+
+	data, pager := topic.Paginate(c, 10)
+	response.JSON(c, gin.H{
+		"data":  data,
+		"pager": pager,
+	})
+}
---
+++ database/factories/topic_factory.go
@@ -0,0 +1,22 @@
+package factories
+
+import (
+	"github.com/go-faker/faker/v4"
+	"gohub/app/models/topic"
+)
+
+func MakeTopics(count int) []topic.Topic {
+	var objs []topic.Topic
+
+	for i := 0; i < count; i++ {
+		topicModel := topic.Topic{
+			Title:      faker.Sentence(),
+			Body:       faker.Paragraph(),
+			CategoryID: "3",
+			UserID:     "1",
+		}
+		objs = append(objs, topicModel)
+	}
+
+	return objs
+}
