f555ae48123669cd6eac10d7a6ac66c672235175
MysticalDevil <jinpeng7981@outlook.com>
2023-01-16 15:50:56 +0800

    Cache affiliate link list

--- app/models/link/link_util.go
+++ app/models/link/link_util.go
@@ -3,8 +3,11 @@ package link
 import (
 	"github.com/gin-gonic/gin"
 	"gohub/pkg/app"
+	"gohub/pkg/cache"
 	"gohub/pkg/database"
+	"gohub/pkg/helpers"
 	"gohub/pkg/paginator"
+	"time"
 )
 
 func Get(idStr string) (link Link) {
@@ -18,23 +21,42 @@ func GetBy(field, value string) (link Link) {
 }
 
 func All() (links []Link) {
-    database.DB.Find(&links)
-    return
+	database.DB.Find(&links)
+	return
 }
 
 func IsExist(field, value string) bool {
-    var count int64
-    database.DB.Model(Link{}).Where("? = ?", field, value).Count(&count)
-    return count > 0
+	var count int64
+	database.DB.Model(Link{}).Where("? = ?", field, value).Count(&count)
+	return count > 0
 }
 
 func Paginate(c *gin.Context, perPage int) (links []Link, paging paginator.Paging) {
-    paging = paginator.Paginate(
-        c,
-        database.DB.Model(Link{}),
-        &links,
-        app.V1URL(database.TableName(&Link{})),
-        perPage,
-    )
-    return
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(Link{}),
+		&links,
+		app.V1URL(database.TableName(&Link{})),
+		perPage,
+	)
+	return
+}
+
+func AllCached() (links []Link) {
+	// Set cache key
+	cacheKey := "links:all"
+	// Set expire time
+	expireTime := 120 * time.Minute
+	// Get data
+	cache.GetObject(cacheKey, &links)
+
+	if helpers.Empty(links) {
+		links = All()
+		if helpers.Empty(links) {
+			return
+		}
+		// Set cache
+		cache.Set(cacheKey, links, expireTime)
+	}
+	return
 }
