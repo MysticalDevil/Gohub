a8af97b2c968f52d9e806405e1b19489fd8bf5bb
MysticalDevil <jinpeng7981@outlook.com>
2023-01-16 14:03:08 +0800

    Authorization policy

---
+++ app/policies/topic_policy.go
@@ -0,0 +1,12 @@
+// Package policies User authorization
+package policies
+
+import (
+	"github.com/gin-gonic/gin"
+	"gohub/app/models/topic"
+	"gohub/pkg/auth"
+)
+
+func CanModifyTopic(c *gin.Context, _topic topic.Topic) bool {
+	return auth.CurrentUID(c) == _topic.UserID
+}
